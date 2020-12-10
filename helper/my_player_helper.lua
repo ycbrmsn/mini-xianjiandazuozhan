6039bc4643d1470b28a0450836a23cc137c06e63
JiangZhiwei <judgezerowit763@163.com>
2020-12-10 23:51:39 +0800

    去掉之前的测试点击事件~~~

--- helper/my_player_helper.lua
+++ helper/my_player_helper.lua
@@ -153,8 +153,6 @@ function MyPlayerHelper:playerClickBlock (objid, blockid, x, y, z)
   PlayerHelper:playerClickBlock(objid, blockid, x, y, z)
   MyStoryHelper:playerClickBlock(objid, blockid, x, y, z)
   -- body
-  SkillHelper:convergeCage(MyPosition:new(x, y + 3, z))
-  -- SkillHelper:constructCage(MyPosition:new(x, y + 3, z))
 end
 
 -- 玩家点击生物
